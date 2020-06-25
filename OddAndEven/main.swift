56f54ab8bedb4e239bd5659f660186c3d482c1a2
Muhammad Debi Sani Abu Bakar Sidik <muhammaddebisani@gmail.com>
2020-06-26 01:31:27 +0700

    Initial Commit

---
+++ OddAndEven/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  OddAndEven
+//
+//  Created by debi sani on 26/06/20.
+//  Copyright © 2020 debi sani. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
