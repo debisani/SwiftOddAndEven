5c2bed2af868b515b99a0bd638c44f37de8e5405
Muhammad Debi Sani Abu Bakar Sidik <muhammaddebisani@gmail.com>
2020-06-27 00:05:41 +0700

    updateCode

--- OddAndEven/main.swift
+++ OddAndEven/main.swift
@@ -10,7 +10,7 @@ import Foundation
 
 print("Selamat Datang di Pencari Nilai Ganjil & Genap")
 print("----------------------------------------------")
-print("Masukan Nilai", terminator: ":"); let value = Int(readLine() ?? "") ?? 0
+print("Masukan Nilai", terminator: ": "); let value = Int(readLine() ?? "") ?? 0
 
 
 print("================> The Result <================")
@@ -20,4 +20,10 @@ if value % 2 == 0 {
     print("Nilai \(value) adalah Bilangan Ganjil")
 }
 
-
+/*
+---> atau kamu bisa menggunakan One Line code dibawah ini untuk mendapatkan hasil/result.
+--------------------------------------------------------------------------------------
+|  let result = value % 2 == 0 ? "Adalah Bilangan Genap" : "Adalah Bilangan Ganjil"  |
+|  print("Nilai \(value), \(result)")                                                |
+--------------------------------------------------------------------------------------
+*/
